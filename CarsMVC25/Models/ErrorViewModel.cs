fc669fa563a9167ae289825d9b103c984d7a1407
peshopbs2 <peshopbs2@gmail.com>
2025-04-05 22:25:00 -0800

    Add project files.

---
+++ CarsMVC25/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarsMVC25.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
